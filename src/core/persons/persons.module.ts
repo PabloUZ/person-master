ba3d831bbb4b6a1e0fc9a12d9cee6cd86961637b
PabloUZ <puribezuluaga28@gmail.com>
2026-08-27 21:58:33 -0500

    chore(persons): scaffold Person service and controller
    Empty CLI-generated scaffolds, wired into PersonsModule. Logic
    (CRUD + duplicate detection) comes next.

--- src/core/persons/persons.module.ts
+++ src/core/persons/persons.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { Person } from './entities/person.entity';
+import { PersonRepository } from './repositories/person.repository';
+import { PersonService } from './services/person/person.service';
+import { PersonController } from './controllers/person/person.controller';
+
+@Module({
+	imports: [TypeOrmModule.forFeature([Person])],
+	providers: [PersonRepository, PersonService],
+	controllers: [PersonController],
+})
 export class PersonsModule {}
